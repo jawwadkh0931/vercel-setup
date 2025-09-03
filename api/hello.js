8cbd432a7aabee0dbcf3a204d2b58e1bf664b5f9
jawwadkh0931 <jawwadk638@gmail.com>
2025-09-03 21:04:34 +0500

    added once more

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,13 @@
 let items = [{ id: 1, name: "Item One" }];
 
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method === "GET") {
     res.status(200).json(items);
   } 
