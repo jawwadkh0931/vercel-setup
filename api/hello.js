ffecac968205c547ad5055c4769fbeb541dc5f45
jawwadkh0931 <jawwadk638@gmail.com>
2025-09-03 20:42:38 +0500

    data added again

--- api/hello.js
+++ api/hello.js
@@ -14,7 +14,17 @@ export default function handler(req, res) {
     const id = parseInt(req.query.id);
     items = items.filter(i => i.id !== id);
     res.status(200).json({ message: "Deleted" });
-  } 
+  }else if (req.method === "PUT") {
+  const id = parseInt(req.query.id);
+  const body = req.body;
+  const item = items.find(i => i.id === id);
+  if (item) {
+    item.name = body.name;
+    res.status(200).json(item);
+    } else {
+        res.status(404).json({ message: "Not found" });
+    }
+}
   else {
     res.status(405).json({ message: "Method not allowed" });
   }
