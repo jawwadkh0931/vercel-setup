a193be6807f2b13e7000df48a656311df0c03d32
jawwadkh0931 <jawwadk638@gmail.com>
2025-09-03 20:38:55 +0500

    data add

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,21 @@
+let items = [{ id: 1, name: "Item One" }];
+
 export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from Vercel API!" });
+  if (req.method === "GET") {
+    res.status(200).json(items);
+  } 
+  else if (req.method === "POST") {
+    const body = req.body;
+    const newItem = { id: items.length + 1, name: body.name };
+    items.push(newItem);
+    res.status(201).json(newItem);
+  } 
+  else if (req.method === "DELETE") {
+    const id = parseInt(req.query.id);
+    items = items.filter(i => i.id !== id);
+    res.status(200).json({ message: "Deleted" });
+  } 
+  else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
 }
